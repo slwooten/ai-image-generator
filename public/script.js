54e66fa39d2a2635897bd74d538ab9a4ab0b500b
Seth Wooten <slwooten14@gmail.com>
2022-12-17 17:22:26 -0500

    post on frontend set up

--- public/script.js
+++ public/script.js
@@ -1,1 +1,40 @@
 console.log('hello there');
+
+const userPrompt = document.getElementById('prompt');
+const generateBtn = document.getElementById('btn');
+const userSize = document.getElementById('size');
+const userQuantity = document.getElementById('quantity');
+
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const prompt = userPrompt.value;
+    const size = userSize.value;
+    const quantity = userQuantity.value;
+    console.log(prompt, size, quantity);
+
+    const url = "/openai/go";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        size: size,
+        quantity: quantity
+      }),
+    };
+    const res = await fetch(url, options);
+
+    const results = await res.json();
+
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generateBtn.addEventListener('click', handleFormSubmit);
